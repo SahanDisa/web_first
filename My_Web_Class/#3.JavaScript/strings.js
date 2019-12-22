4ee36cc8c6e2293f20ee41698370e183ebd54499
Sahan Dissanayaka <t.sahan998@gmail.com>
2019-12-22 11:43:54 +0530

    Added Strings in JS

---
+++ My_Web_Class/#3.JavaScript/strings.js
@@ -0,0 +1,31 @@
+//strings
+//strings are sequence of characters
+var strName = "Hey! I'm using Javascript";
+
+//strings accepts both single quotes and double quotes
+var single = 'Hi Js single string';
+var double = 'Hi Js double string';
+
+//string operations
+//string length
+var strlen = strName.length;
+console.log(strlen);
+
+//string concatation
+var newwe = strName.concat(single);
+console.log(newwe);
+//using + operator
+var string3 = single + double;
+
+//Uppercase
+var Mystring = "Don't mess up with my strings";
+Mystring.toUpperCase;
+console.log(Mystring.toUpperCase);
+
+//Lowercase
+Mystring.toLowerCase;
+console.log(Mystring.toLowerCase);
+
+//substring
+var MyNewStr = "I'm a beginner to Javascript";
+MyNewStr.substr(5,7);
